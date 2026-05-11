3acdb5fc990081736c7871a10021f448c4827a8e
pr59697989-sys <pr59697989@gmail.com>
2026-05-11 20:52:42 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URL = os.getenv("MONGO_URL")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS").split(",")))
